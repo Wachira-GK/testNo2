5fd31f7f622441457445626b07ab1588f0bb6413
Wachira-GK <godmarklee@gmail.com>
2022-08-15 04:01:49 +0300

    test5

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         app 1 by GK
+         app 1 by GK deploy
         </p>
         <a
           className="App-link"
